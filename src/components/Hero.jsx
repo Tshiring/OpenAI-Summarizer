e2557b7c63185f9e537136546a923c537103b02b
Tshiring <tshiring1814@gmail.com>
2023-04-26 12:43:13 +0545

    initial push

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+	return (
+		<header className="w-full flex justify-center items-center flex-col">
+			<nav className="flex justify-between items-center w-full mb-10 pt-3">
+				<img src={logo} className="w-28 object-contain" alt="sumz_logo" />
+				<button
+					type="button"
+					onClick={() => window.open('https://github.com/Tshiring')}
+					className="black_btn"
+				>
+					Github
+				</button>
+			</nav>
+			<h1 className="head_text">
+				Summarize Articles with <br className="max-md:hidden" />
+				<span className="orange_gradient">OpenAI GPT-4</span>
+			</h1>
+			<h2 className="desc">
+				Simplify your reading with Summize, an open-source article summarizer
+				that transforms lengthy articles into clear and concise summaries
+			</h2>
+		</header>
+	);
+};
+
+export default Hero;
